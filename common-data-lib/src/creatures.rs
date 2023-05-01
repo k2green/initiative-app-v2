d4f4fd9952f6bdf212996eeab9688e1797214fae
Kyle Green <kyle.green999@gmail.com>
2023-05-01 12:05:55 +0100

    added common lib

---
+++ common-data-lib/src/creatures.rs
@@ -0,0 +1,214 @@
+use std::{cmp::Ordering, slice::{Iter, IterMut}};
+
+use serde::{Serialize, Deserialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub enum OrderMode {
+    #[default]
+    Alphabetical,
+    Initiative
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Default)]
+pub struct CreatureContainer {
+    ordering: OrderMode,
+    creatures: Vec<Creature>
+}
+
+impl From<Vec<Creature>> for CreatureContainer {
+    fn from(mut value: Vec<Creature>) -> Self {
+        value.sort_by(alphabetical);
+
+        Self {
+            ordering: OrderMode::Alphabetical,
+            creatures: value
+        }
+    }
+}
+
+impl CreatureContainer {
+    pub fn sort(&mut self) {
+        match self.ordering {
+            OrderMode::Alphabetical => self.creatures.sort_by(alphabetical),
+            OrderMode::Initiative => self.creatures.sort()
+        }
+    }
+
+    pub fn set_order_mode(&mut self, mode: OrderMode) {
+        if mode != self.ordering {
+            self.ordering = mode;
+            self.sort();
+        }
+    }
+
+    pub fn get_index_from_id(&self, id: Uuid) -> Option<usize> {
+        self.creatures.iter()
+            .enumerate()
+            .find_map(|(idx, creature)| {
+                if creature.id == id {
+                    Some(idx)
+                } else {
+                    None
+                }
+            })
+    }
+
+    pub fn insert(&mut self, creature: Creature) {
+        self.creatures.push(creature);
+        self.sort();
+    }
+
+    pub fn get_by_index(&self, index: usize) -> Option<&Creature> {
+        self.creatures.get(index)
+    }
+
+    pub fn get_mut_by_index(&mut self, index: usize) -> Option<&mut Creature> {
+        self.creatures.get_mut(index)
+    }
+
+    pub fn remove_by_index(&mut self, index: usize) -> Creature {
+        self.creatures.remove(index)
+    }
+
+    pub fn get(&self, id: Uuid) -> Option<&Creature> {
+        let index = self.get_index_from_id(id)?;
+        self.get_by_index(index)
+    }
+
+    pub fn get_mut(&mut self, id: Uuid) -> Option<&mut Creature> {
+        let index = self.get_index_from_id(id)?;
+        self.get_mut_by_index(index)
+    }
+
+    pub fn remove(&mut self, id: Uuid) -> Option<Creature> {
+        let index = self.get_index_from_id(id)?;
+        Some(self.remove_by_index(index))
+    }
+
+    pub fn iter(&self) -> Iter<Creature> {
+        self.creatures.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<Creature> {
+        self.creatures.iter_mut()
+    }
+
+    pub fn cloned(&self) -> Vec<Creature> {
+        self.creatures.clone()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Creature {
+    id: Uuid,
+    name: String,
+    selected: bool,
+    initiative: isize,
+    sub_order: isize
+}
+
+impl<T: ToString> From<T> for Creature {
+    fn from(value: T) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name: value.to_string(),
+            selected: false,
+            initiative: 0,
+            sub_order: 0
+        }
+    }
+}
+
+impl PartialOrd for Creature {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.initiative.partial_cmp(&other.initiative) {
+            Some(Ordering::Equal) => {},
+            ord => return ord
+        };
+
+        match self.sub_order.partial_cmp(&other.sub_order) {
+            Some(Ordering::Equal) => {},
+            ord => return ord
+        }
+
+        self.name.to_lowercase().partial_cmp(&other.name.to_lowercase())
+    }
+}
+
+impl Ord for Creature {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.initiative.cmp(&other.initiative) {
+            Ordering::Equal => {},
+            ord => return ord
+        };
+
+        match self.sub_order.cmp(&other.sub_order) {
+            Ordering::Equal => {},
+            ord => return ord
+        }
+
+        self.name.to_lowercase().cmp(&other.name.to_lowercase())
+    }
+}
+
+impl Creature {
+    pub fn id(&self) -> Uuid {
+        self.id
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn selected(&self) -> bool {
+        self.selected
+    }
+
+    pub fn set_selected(&mut self, value: bool) {
+        self.selected = value;
+    }
+
+    pub fn initiative(&self) -> isize {
+        self.initiative
+    }
+
+    pub fn set_initiative(&mut self, value: isize) {
+        self.initiative = value;
+    }
+
+    pub fn sub_order(&self) -> isize {
+        self.sub_order
+    }
+
+    pub fn set_sub_order(&mut self, value: isize) {
+        self.sub_order = value;
+    }
+}
+
+fn alphabetical(a: &Creature, b: &Creature) -> Ordering {
+    a.name.to_lowercase().cmp(&b.name.to_lowercase())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialization() {
+        let creature = Creature::from("Test creature");
+        let serialized = serde_json::to_string_pretty(&creature).unwrap();
+
+        println!("{}", serialized);
+    }
+
+    #[test]
+    fn test_deserialization() {
+        let creature = Creature::from("Test creature");
+        let serialized = serde_json::to_string_pretty(&creature).unwrap();
+        let deserialized: Creature = serde_json::from_str(&serialized).unwrap();
+
+        println!("{:#?}", deserialized);
+        assert_eq!(creature, deserialized);
+    }
+}
---
+++ common-data-lib/src/lib.rs
@@ -0,0 +1,47 @@
+use serde::{Deserialize, Serialize};
+
+pub mod creatures;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BackendError {
+    InternalError(String),
+    ArgumentError{ argument_name: String, message: String},
+}
+
+impl std::fmt::Display for BackendError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InternalError(e) => write!(f, "Internal Error: {}", e),
+            Self::ArgumentError { argument_name, message } => write!(f, "Error with argument '{}': {}", argument_name, message),
+        }
+    }
+}
+
+impl<T: std::error::Error> From<T> for BackendError {
+    fn from(value: T) -> Self {
+        Self::InternalError(value.to_string())
+    }
+}
+
+impl BackendError {
+    pub fn internal_error(message: impl Into<String>) -> Self {
+        Self::InternalError(message.into())
+    }
+
+    pub fn argument_error(arg_name: impl Into<String>, message: impl Into<String>) -> Self {
+        Self::ArgumentError {
+            argument_name: arg_name.into(),
+            message: message.into()
+        }
+    }
+}
+
+pub trait ToBackendResult<T> {
+    fn to_backend_result(self) -> Result<T, BackendError>;
+}
+
+impl<T, E: std::error::Error> ToBackendResult<T> for Result<T, E> {
+    fn to_backend_result(self) -> Result<T, BackendError> {
+        self.map_err(|e| BackendError::from(e))
+    }
+}
